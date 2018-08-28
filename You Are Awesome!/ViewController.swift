adbff0925d0e0215ec745b1e8d7356298c5feaa6
Mary Nugent <gn20895@aol.com>
2018-08-28 12:36:03 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Brennan Nugent on 8/28/18.
+//  Copyright © 2018 Brennan Nugent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
